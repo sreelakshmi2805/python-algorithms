cec3f76a6fe3de12e88237a8c423850b8691b1f7
sreelakshmi2805 <57938123+sreelakshmi2805@users.noreply.github.com>
2020-10-14 16:15:50 +0530

    Add files via upload

---
+++ large_min.py
@@ -0,0 +1,14 @@
+def large_small(arr):
+    large=arr[0]
+    small=arr[0]
+    for i in range(len(arr)):
+        if arr[i]>large:
+            large=arr[i]
+        elif arr[i]<small:
+            small=arr[i]
+    print("largest element is", large)
+    print("smallest element is", small)
+    
+arr=[34,76,90,89,56,98]
+large_small(arr)
+
---
+++ sec_large.py
@@ -0,0 +1,16 @@
+def largest(arr):
+    large=arr[0]
+    sec_large=arr[0]
+    for i in range(1,len(arr)):
+        if arr[i]>large:
+            sec_large=large
+            large=arr[i]
+        elif arr[i]>sec_large:
+            sec_large=arr[i]
+    print("largest element is",large)
+    print("second largest element is",sec_large)
+
+
+arr=[34,76,90,89,56,98]
+largest(arr)
+
